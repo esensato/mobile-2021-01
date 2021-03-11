40696823c8f1d099d88a850e93e1d6b3e08f9beb
esensato <esensato@hotmail.com>
2021-03-11 16:13:42 -0300

    Memo Colors

---
+++ MemoColors/app/src/main/java/com/example/memocolors/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.example.memocolors
+
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    // contém a sequencia de cores sortedas
+    lateinit var sorteados:IntArray
+    // timer que controla o tempo entre sorteios
+    lateinit var timer:Timer
+    // quantidade de cores sorteadas
+    var repeticoes = 1
+    // posição do item sorteado no array
+    var pos = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txtRepeticoes.text = "$repeticoes"
+
+    }
+
+    fun iniciar(view:View) {
+
+        // efetuar o sorteio da sequencia dos botoes
+        sorteados = sortear()
+        pos = 0
+        timer = Timer()
+        var apagar = true
+        var corOriginal = 0
+        timer.schedule(object: TimerTask() {
+               override fun run() {
+                   if (pos == sorteados.size) {
+                       timer.cancel()
+                       pos = 0
+                       Log.i("SORTEIO", "Cancelado timer")
+                   } else {
+                       Log.i("SORTEIO", "pos = ${sorteados.get(pos)}")
+                       // obtem a referencia ao botao que ira "piscar"
+                       val btn = obterBotao(sorteados.get(pos))
+                       if (apagar) {
+                           corOriginal = (btn.background as ColorDrawable).color
+                           runOnUiThread {
+                               btn.setBackgroundColor(Color.WHITE)
+
+                           }
+                           apagar = false
+
+                       } else {
+                               runOnUiThread {
+                                   btn.setBackgroundColor(corOriginal)
+                               }
+
+                               apagar = true
+                               pos++
+
+                       }
+
+                   }
+               }
+               }, 0, 1000)
+
+
+    }
+
+    // sorteia uma quantidade de numeros (0..3) baseado na quantidade de repeticoes atual
+    fun sortear():IntArray {
+
+        var ret = IntArray(repeticoes)
+        for (i in 0..ret.size - 1) {
+            ret[i] = Random.nextInt(4)
+        }
+
+        return ret
+    }
+
+    fun obterBotao(numero:Int):ImageButton {
+        val ret = when (numero) {
+            0 -> btnCima
+            1 -> btnDireita
+            2 -> btnBaixo
+            else -> btnEsquerda
+        }
+
+        return ret
+    }
+
+    fun selecionar(view:View) {
+
+        if (view.id == obterBotao(sorteados[pos]).id) {
+            Log.i("SELECIONAR", "Correto")
+            pos++
+            if (pos == sorteados.size) {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(R.string.app_name)
+                builder.setIcon(R.drawable.baseline_check_circle_black_18dp)
+                builder.setMessage(R.string.lblCorretoDialog)
+                builder.show()
+                repeticoes++
+                pos = 0
+                btnIniciar.isEnabled = true
+                txtRepeticoes.text = "$repeticoes"
+            }
+        } else {
+            Log.i("SELECIONAR", "Errado")
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.app_name)
+            builder.setIcon(R.drawable.baseline_error_black_18dp)
+            builder.setMessage(R.string.lblErroDialog)
+            builder.show()
+            repeticoes = 1
+            pos = 0
+            btnIniciar.isEnabled = true
+            txtRepeticoes.text = "$repeticoes"
+        }
+
+
+
+    }
+}
