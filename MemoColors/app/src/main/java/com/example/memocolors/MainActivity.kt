44b82c71cc0e239970902f2e938a34ba855db648
esensato <esensato@hotmail.com>
2021-03-11 15:59:50 -0300

    Tabuada

--- MemoColors/app/src/main/java/com/example/memocolors/MainActivity.kt
+++ MemoColors/app/src/main/java/com/example/memocolors/MainActivity.kt
@@ -6,8 +6,6 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -20,26 +18,25 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    // contém a sequencia de cores sortedas
     lateinit var sorteados:IntArray
+    // timer que controla o tempo entre sorteios
     lateinit var timer:Timer
+    // quantidade de cores sorteadas
     var repeticoes = 1
+    // posição do item sorteado no array
     var pos = 0
-    lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         txtRepeticoes.text = "$repeticoes"
-        handler = Handler(mainLooper)
 
     }
 
-
-
     fun iniciar(view:View) {
 
-        //(view as Button).isEnabled = false
-
+        // efetuar o sorteio da sequencia dos botoes
         sorteados = sortear()
         pos = 0
         timer = Timer()
@@ -53,8 +50,9 @@ class MainActivity : AppCompatActivity() {
                        Log.i("SORTEIO", "Cancelado timer")
                    } else {
                        Log.i("SORTEIO", "pos = ${sorteados.get(pos)}")
+                       // obtem a referencia ao botao que ira "piscar"
+                       val btn = obterBotao(sorteados.get(pos))
                        if (apagar) {
-                           val btn = obterBotao(sorteados.get(pos))
                            corOriginal = (btn.background as ColorDrawable).color
                            runOnUiThread {
                                btn.setBackgroundColor(Color.WHITE)
@@ -63,9 +61,6 @@ class MainActivity : AppCompatActivity() {
                            apagar = false
 
                        } else {
-                           try {
-
-                               var btn = obterBotao(sorteados.get(pos))
                                runOnUiThread {
                                    btn.setBackgroundColor(corOriginal)
                                }
@@ -73,16 +68,6 @@ class MainActivity : AppCompatActivity() {
                                apagar = true
                                pos++
 
-                           } catch (e:Exception) {
-                               handler.post {
-                                   val builder = AlertDialog.Builder(baseContext)
-                                   builder.setTitle(R.string.app_name)
-                                   builder.setIcon(R.drawable.baseline_error_black_18dp)
-                                   builder.setMessage(e.message)
-                                   builder.show()
-
-                               }
-                           }
                        }
 
                    }
@@ -92,11 +77,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // sorteia uma quantidade de numeros (0..3) baseado na quantidade de repeticoes atual
     fun sortear():IntArray {
 
         var ret = IntArray(repeticoes)
         for (i in 0..ret.size - 1) {
-            ret[i] = Random.nextInt(3)
+            ret[i] = Random.nextInt(4)
         }
 
         return ret
